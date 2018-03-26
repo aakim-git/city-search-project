af105cd6e55ae97ae4f254f92b2d844cf25bd0c0
Phil Choi <philchoi1@gmail.com>
2018-03-26 14:43:44 -0700

    commit as of 3/26

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
 
-//set API URLs to variables 
-URL_FOURSQUARE = 'https://api.foursquare.com/v2/venues/explore';
-// URL_FOURSQUARE2 = 'https://api.foursquare.com/v2/venues/categories';
-URL_METAWEATHER = 'https://www.metaweather.com/api/location/(woeid)/';
-
+//set API URL to variable 
+const URL_FOURSQUARE = 'https://api.foursquare.com/v2/venues/explore';
 
 // Get city data from Foursquare API
 function getDataFromFourSquareAPI (city, category, callback) {
@@ -12,16 +9,16 @@ function getDataFromFourSquareAPI (city, category, callback) {
         near: `${city}`,
         section: `${category}`,
         query: 'recommended',
-        radius: 30,
+        radius: 100,
         client_id: 'N4TONANM1I0FXAJRPD414MFYR0BYIMLAEKRCLNMEUAMRJ0VR',
         client_secret: 'ZEYA0Z2LOFLOZ321VQYJKVMHTN5BQFCBZ1SYIMFJQ4ZQ3LUC',
         v: 20180320,
-        limit: 20,
-        venuePhotos: 1
+        limit: 18,
+        venuePhotos: 1,
     };  
 
     $.getJSON(URL_FOURSQUARE, parameters, callback);
-    // https://api.foursquare.com/v2/venues/explore?near=Davis,CA&section=food&query=tacos&client_id=N4TONANM1I0FXAJRPD414MFYR0BYIMLAEKRCLNMEUAMRJ0VR&client_secret=ZEYA0Z2LOFLOZ321VQYJKVMHTN5BQFCBZ1SYIMFJQ4ZQ3LUC&v=20180320&limit=20&venuePhotos=1    // $.ajax(settings, callback);
+    // https://api.foursquare.com/v2/venues/explore?near=Davis,CA&section=food&query=tacos&client_id=N4TONANM1I0FXAJRPD414MFYR0BYIMLAEKRCLNMEUAMRJ0VR&client_secret=ZEYA0Z2LOFLOZ321VQYJKVMHTN5BQFCBZ1SYIMFJQ4ZQ3LUC&v=20180320&limit=20&venuePhotos=1 
 }
 
 
@@ -36,25 +33,44 @@ function displayFourSquareData(data) {
 // render the results to HTML
 function renderResult (result) {
     return `
-    <div class="container-results">
-        <div class="venue-name">
-            <a href="${result.venue.url}">${result.venue.name}</a>
-            <img class="venue-image" src="${getImageURL(result.venue)}">
-            <img class="venue-category-logo" src="${result.venue.categories[0].icon.prefix}bg_32${result.venue.categories[0].icon.suffix}" alt="category logo">
-            <p class="venue-category-name">${result.venue.categories[0].shortName}</p>
+    <div class="col-4 ${hiddenElementNoPictures(result.venue)}">
+        <div class="venue-card">
+            <div class="venue-name">    
+                <a href="${result.venue.url}">${result.venue.name}</a>
+            </div>
+            <a href="${result.venue.url}" target="_blank"><img class="venue-image" src="${getImageURL(result.venue)}"></a>
+            <div class="venue-category">
+                <img class="venue-category-logo" src="${result.venue.categories[0].icon.prefix}bg_32${result.venue.categories[0].icon.suffix}" alt="category logo">
+                <p class="venue-category-name">${result.venue.categories[0].shortName}</p>
+            </div>
+            <div class="venue-address-container">
+                <p class="venue-address-street">Address: ${result.venue.location.formattedAddress}</p>
+                <p class="venue-address-street">${result.tips[0].text}</p>
+        
+            </div>
         </div>
     </div>`;
+}
+
+    // <p class="venue-address-city">City: ${result.venue.location.city}, ${result.venue.location.state}</p>
+    //     <p class="venue-address-phone">Telephone: ${result.venue.contact.phone}</p>
+
+function hiddenElementNoPictures (venue) {
+    if (venue.photos.count === 0) {
+        return 'hidden-element';
+    } else {
+        return "";
+    };
 }
 
 
 //get image URL
 function getImageURL (venue) {
-    if (venue.photos.count > 0) {
+    if (venue.photos.count === 1) {
         let photoObj = venue.photos.groups[0].items[0];
         let prefix = photoObj.prefix;
         let suffix = photoObj.suffix;
-        let imageSize = (photoObj.width+'x'+photoObj.height);
-        return prefix + imageSize + suffix;
+        return prefix + "250x250" + suffix;
     } else {
         return 'none';
     };
@@ -62,15 +78,14 @@ function getImageURL (venue) {
   
 //user text input based on which button you press
 function submitCategoryButtons() {
-    $('#searchForm .buttons button').on('click', function (ev) {
-        ev.preventDefault();
+    $('#searchForm .buttons button').on('click', function (e) {
+        e.preventDefault();
         let inputTarget = $('#searchInput').val();
-        let category = $(ev.currentTarget).data('4sq-category');
+        let category = $(e.currentTarget).data('4sq-category');
         getDataFromFourSquareAPI(inputTarget, category, displayFourSquareData);
+        searchWeather(query);
     });
 }
-
-
 
 submitCategoryButtons();
 
